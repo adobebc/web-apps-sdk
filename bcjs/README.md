41f041ee5577dd6ad0fa9bae58ac2b4388edb0cf
Radu Viorel Cosnita <cosnita@adobe.com>
2013-09-03 13:57:20 +0300

    I removed bcapi subtree from sample app.

--- bcjs/README.md
+++
@@ -1,4 +0,0 @@
-bcapi.js
-========
-
-This is a sample library for working with Business Catalyst API.
--- bcjs/test/sample_test.js
+++
@@ -1,5 +0,0 @@
-describe("Sample Test Cases", function() {
-	it("extremely simple assert", function() {
-		expect(1).toBe(1);
-	});
-});

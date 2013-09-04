12444c0206db4e0d0f61a0fbf2bd962ef1a8b255
Radu Viorel Cosnita <cosnita@adobe.com>
2013-09-04 16:33:10 +0300

    I removed all classes and code. Now only the stubs are available.

--- src/country.js
+++ src/country.js
@@ -1,19 +1,4 @@
 (function($) {
 	"use strict";
 	
-    BCAPI.Country = function(attributes) {
-        this.setAttributes(attributes);
-    };
-
-    $.extend(BCAPI.Country, {
-        uri: function() { return '/api/v2/admin/system/countries'; },
-
-        list: BCAPI.FactoryCRUD.list,
-        listForIp: function(ip) {
-            return fetchList(this.uri() + '?ip=' + (ip || 'current'), new BCAPI.Paginator(Country));
-        }
-    });
-
-    $.extend(BCAPI.Country.prototype, BCAPI.EntityBase);
-	
 })(jQuery);
--- src/webapp_item.js
+++ src/webapp_item.js
@@ -1,43 +1,4 @@
 (function($) {
 	"use strict";
-	
-    BCAPI.WebAppItem = function(webApp, attributes) {
-        this.webApp = webApp;
-        this.setAttributes(attributes);
-    };
 
-    $.extend(BCAPI.WebAppItem, {
-        uri: function(webApp) {
-            return webApp.uri() + "/items";
-        },
-
-        create: BCAPI.WebAppField.create,
-        get: BCAPI.WebAppField.get,
-
-        // Find items corresponding to the criteria specified in the <query> parameter.
-        // The <query> can be either an URL query string or a key-value object.
-        list: function(webApp, query) {
-            var uri = this.uri(webApp);
-            if (query) uri += '?' + (typeof query === "object" ? $.param(query) : query);
-            
-            return fetchList(uri, new BCAPI.Paginator(this, webApp));
-        }
-    });
-
-    $.extend(BCAPI.WebAppItem.prototype, BCAPI.EntityCRUD, {
-        uri: function() {
-            return BCAPI.WebAppItem.uri(this.webApp) + "/" + this.attributes.id;
-        },
-        categoriesUri: function() {
-            return this.uri() + "/categories";
-        },
-
-        getCategories: function() {
-            return fetchList(this.categoriesUri());
-        },
-        saveCategories: function(categories) {
-            return request('PUT', this.categoriesUri(), categories);
-        }
-    });
-	
 })(jQuery);

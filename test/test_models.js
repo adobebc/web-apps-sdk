3ca6d4c68fb3475d8c9142fd3a0652795b7f1a2c
Radu Viorel Cosnita <cosnita@adobe.com>
2013-09-05 14:43:59 +0300

    I added the first unit test and mocks namespace for BCAPI.

--- test/test_models.js
+++ test/test_models.js
@@ -1,3 +1,23 @@
-describe("Uni tests for BC models namespace.", function() {
-	describe()
+describe("Unit tests for BC models namespace.", function() {
+	describe("Unit tests for BC base model class.", function() {
+		var oldSiteHelper;
+		
+		beforeEach(function() {
+			oldSiteHelper = BCAPI.Helpers.Site;
+			
+			BCAPI.Mocks.Site.mock(undefined, "123");
+		});
+		
+		afterEach(function() {
+			BCAPI.Helpers.Site = oldSiteHelper;
+		});
+		
+		it("Check default authorization site header", function() {
+			var model = new BCAPI.Models.Model();
+			
+			var headers = model.headers();
+			
+			expect(headers.Authorization).toBe("123");
+		});
+	});
 });

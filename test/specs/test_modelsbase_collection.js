31cede2198c29a1290f3fa1d0d1b0d1ecd7ee94a
Radu Viorel Cosnita <cosnita@adobe.com>
2013-09-09 12:01:14 +0300

    I fixed a bug in url built for collections: & was no separating query params. I added unit tests for fetch with order / pagination specified.

--- test/specs/test_modelsbase_collection.js
+++ test/specs/test_modelsbase_collection.js
@@ -30,13 +30,23 @@ describe("Unit tests for BC base collection class.", function() {
 		expect(collection.url()).toBe(person.urlRoot());
 	});
 	
-	it("Check correct collection fetch with default values..", function() {
+	/**
+	 * This method provides a test template for collection fetch.
+	 * 
+	 * @params {List} expectedItems A list of items containing person data.
+	 * @param {Object} options Options required for filtering, ordering, pagination.
+	 * @param {Integer} numQueryParams The total number of query params we expect api url to have.
+	 * @param {Integer} options.skip The number of records we want to skip from data set.
+	 * @param {Integer} options.limit The total number of records we want to include in data set.
+	 * @param {String} options.order The order expression used.
+	 */
+	function _testCollectionFetchWithParams(expectedItems, numQueryParams, options) {
 		var personModel = new BCAPI.Mocks.Models.PersonModel(),
-			collection = new BCAPI.Mocks.Models.PersonCollection(),
-			expectedItems = [{"firstName": "John",
-				 			  "lastName": "Doe"}], 
+			collection = new BCAPI.Mocks.Models.PersonCollection(), 
 			callbackCalled = false;
-
+		
+		options = options || {};
+		
 		function successHandler(returnedCollection, response, options) {
 			var idx = 0;
 			
@@ -51,11 +61,23 @@ describe("Unit tests for BC base collection class.", function() {
 		}
 		
 		spyOn($, "ajax").andCallFake(function(request) {
-			var urlRoot = personModel.urlRoot();
+			var urlRoot = personModel.urlRoot(),
+				expectedLimit = options.limit || BCAPI.Config.Pagination.limit,
+				expectedSkip = options.skip || BCAPI.Config.Pagination.skip,
+				expectedOrder = options.order,
+				totalAnd = request.url.split("&").length;
 			
 			expect(request.url.substring(0, urlRoot.length + 1)).toBe(urlRoot + "?");
-			expect(request.url).toContain("limit=" + BCAPI.Config.Pagination.limit);
-			expect(request.url).toContain("skip=" + BCAPI.Config.Pagination.skip);
+			expect(request.url).toContain("limit=" + expectedLimit);
+			expect(request.url).toContain("skip=" + expectedSkip);
+			
+			if(expectedOrder) {
+				expect(request.url).toContain("order=" + expectedOrder);
+			} else {
+				expect(request.url).not.toContain("order=");
+			}
+			
+			expect(totalAnd).toBe(numQueryParams);			
 			expect(request.type).toBe("GET");
 			expect(request.dataType).toBe("json");
 			expect(request.headers.Authorization).toBe(siteToken);
@@ -64,13 +86,43 @@ describe("Unit tests for BC base collection class.", function() {
 		});
 		
 		runs(function() {
-			collection.fetch({
-				success: successHandler
-		    });
+			var fetchArgs = {success: successHandler};
+
+			for(var key in options) {
+				fetchArgs[key] = options[key];
+			}
+			
+			collection.fetch(fetchArgs);
 		});
 		
 		waitsFor(function() {
 			return callbackCalled;
-		}, "Success callback not called.", 50);
+		}, "Success callback not called.", 50);		
+	}
+	
+	it("Check correct collection fetch with default values..", function() {
+		var expectedItems = [{"firstName": "John",
+			  				  "lastName": "Doe"},
+			  				 {"firstName": "Triple",
+			  			      "lastName": "X"}];
+		
+		_testCollectionFetchWithParams(expectedItems, 2);
+	});
+	
+	it("Check correct collection fetch pagination.", function() {
+		var expectedItems = [{"firstName": "John",
+							  "lastName": "Doe"}],
+			options = {skip: 10, limit: 1};
+							  
+		_testCollectionFetchWithParams(expectedItems, 2, options);
+	});
+	
+	it("Check correct collection fetch ordering.", function() {
+		var expectedItems = [{"firstName": "Triple", "lastName": "X"},
+		                     {"firstName": "John", "lastName": "Doe"}];
+		
+		options = {"order": "-lastName"};
+		
+		_testCollectionFetchWithParams(expectedItems, 3, options);
 	});
 });

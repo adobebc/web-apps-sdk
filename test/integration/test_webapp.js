7c22472ae749d978c3e0d6fc69c2b72b043dbbfe
Sandu Lungu <sandu@lungu.info>
2013-09-09 17:25:59 +0300

    Updated integration tests helper

--- test/integration/test_webapp.js
+++ test/integration/test_webapp.js
@@ -1,39 +1,44 @@
 describe("Helper.Models.WebApp", function() {
-        it("CRUD", function() {
+    beforeEach(function() {
+        BCAPI.Helper.Test.useTestServer();
+    });
 
-            BCAPI.Helper.Test.useTestServer();
+    it("Create, Read, Delete", function() {
+        var webApp, saved = false;
 
-            var webApp, saved = false;
-
-            runs(function() {
-                webApp = new BCAPI.Models.WebApp({name: "FirstWebAppFromApi"});
-                webApp.save().done(function() { saved = true });
-            });
-
-            waitsFor(function() {
-                return saved;
-            }, 'Create WebApp', 500);
-
-            var fetched = false;
-
-            runs(function() {
-                var webApp = new BCAPI.Models.WebApp({name: "FirstWebAppFromApi"});
-                webApp.fetch().done(function() { fetched = true });
-            });
-
-            waitsFor(function() {
-                return fetched;
-            }, 'Read WebApp', 500);
-
-            var deleted = false;
-
-            runs(function() {
-                var webApp = new BCAPI.Models.WebApp({name: "FirstWebAppFromApi"});
-                webApp.destroy().done(function() { deleted = true });
-            });
-
-            waitsFor(function() {
-                return deleted;
-            }, 'Read WebApp', 500);
+        runs(function() {
+            webApp = new BCAPI.Models.WebApp({name: "FirstWebAppFromApi"});
+            webApp.save().done(function() { saved = true });
         });
+
+        waitsFor(function() {
+            return saved;
+        }, 'Create WebApp', 500);
+
+        var fetched = false;
+
+        runs(function() {
+            var webApp = new BCAPI.Models.WebApp({name: "FirstWebAppFromApi"});
+            webApp.fetch().done(function() { fetched = true });
+        });
+
+        waitsFor(function() {
+            return fetched;
+        }, 'Read WebApp', 500);
+
+        // TODO: Test webApp has correct attributes
+
+        var deleted = false;
+
+        runs(function() {
+            var webApp = new BCAPI.Models.WebApp({name: "FirstWebAppFromApi"});
+            webApp.destroy().done(function() { deleted = true });
+        });
+
+        waitsFor(function() {
+            return deleted;
+        }, 'Delete WebApp', 500);
+
+        // TODO: Test webApp was deleted
+    });
 });

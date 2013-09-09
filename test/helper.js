97bb2b75a0350f6fafea87253dc26411d333db32
Sandu Lungu <sandu@lungu.info>
2013-09-09 17:25:59 +0300

    Updated integration tests helper

--- test/helper.js
+++ test/helper.js
@@ -1,62 +1,85 @@
 (function($) {
     "use strict";
 
+    var config = {
+        rootUrl: 'https://api-dc1.secure-earth.bc.corp.adobe.com/',
+        siteId: 38604,
+        username: 'bcadmin@bc1.local', //'bcapi-integration@mailinator.com',
+        password: 'password9', // '123456'
+        genericToken: null,
+        siteToken: null
+    };
+
+    function mockGenericToken() {
+        spyOn(BCAPI.Helper.Site, "getGenericToken").andReturn(config.genericToken);
+    }
+    function mockSiteToken() {
+        spyOn(BCAPI.Helper.Site, "getSiteToken").andReturn(config.siteToken);
+    }
+
+    BCAPI.Config.TestServer = config;
+
     BCAPI.Helper.Test = {
         useTestServer: function() {
-            if (!BCAPI.Helper.Site.getRootUrl.isSpy()) {
-                spyOn(BCAPI.Helper.Site, "getRootUrl").andReturn('https://api-dc1.secure-earth.bc.corp.adobe.com/');
+            if (!BCAPI.Helper.Site.getRootUrl.isSpy) {
+                spyOn(BCAPI.Helper.Site, "getRootUrl").andReturn(config.rootUrl);
             };
 
             if (!BCAPI.Helper.Site.getSiteId.isSpy) {
-                apyOn(BCAPI.Helper.Site, 'getSiteId').andReturn(38604);
+                apyOn(BCAPI.Helper.Site, 'getSiteId').andReturn(config.siteId);
             }
 
-            var genericToken = '',
-                siteToken = '';
-
-            if (!BCAPI.Helper.Site.getGenericToken.isSpy()) {
-                runs(function() {
-                    $
-                        .ajax({
-                            type: 'POST',
-                            url: BCAPI.Helper.Site.getRootUrl() + '/api/v2/admin/tokens',
-                            contentType: 'application/json',
-                            data: JSON.stringify({
-                                username: 'bcadmin@bc1.local', //'bcapi-integration@mailinator.com',
-                                password: 'password9' // '123456'
+            if (!BCAPI.Helper.Site.getGenericToken.isSpy) {
+                if (config.genericToken) {
+                    mockGenericToken();
+                } else {
+                    runs(function() {
+                        $
+                            .ajax({
+                                type: 'POST',
+                                url: BCAPI.Helper.Site.getRootUrl() + '/api/v2/admin/tokens',
+                                contentType: 'application/json',
+                                data: JSON.stringify({
+                                    username: config.username,
+                                    password: config.password
+                                })
                             })
-                        })
-                        .done(function(data) {
-                            genericToken = data.token;
-                            spyOn(BCAPI.Helper.Site, "getGenericToken").andReturn(genericToken);
-                        });
-                });
+                            .done(function(data) {
+                                config.genericToken = data.token;
+                                mockGenericToken();
+                            });
+                    });
+                }
 
                 waitsFor(function() {
-                    return !!genericToken;
+                    return !!config.genericToken;
                 }, 'Get genericToken', 10 * 1000);
             }
 
-            if (!BCAPI.Helper.Site.getSiteToken.isSpy()) {
-                runs(function() {
-                    $
-                        .ajax({
-                            type: 'POST',
-                            url: BCAPI.Helper.Site.getRootUrl() + '/api/v2/admin/sites/' + BCAPI.Helper.Site.getSiteId() + '/tokens',
-                            contentType: 'application/json',
-                            headers: {
-                                Authorization: BCAPI.Helper.Site.getGenericToken()
-                            }
-                        })
-                        .done(function(data) {
-                            siteToken = data.token;
-                            spyOn(BCAPI.Helper.Site, "getSiteToken").andReturn(siteToken);
-                        });
-                });
+            if (!BCAPI.Helper.Site.getSiteToken.isSpy) {
+                if (config.genericToken) {
+                    mockSiteToken();
+                } else {
+                    runs(function() {
+                        $
+                            .ajax({
+                                type: 'POST',
+                                url: BCAPI.Helper.Site.getRootUrl() + '/api/v2/admin/sites/' + BCAPI.Helper.Site.getSiteId() + '/tokens',
+                                contentType: 'application/json',
+                                headers: {
+                                    Authorization: BCAPI.Helper.Site.getGenericToken()
+                                }
+                            })
+                            .done(function(data) {
+                                config.siteToken = data.token;
+                                mockSiteToken();
+                            });
+                    });
 
-                waitsFor(function() {
-                    return !!siteToken;
-                }, 'Get siteToken', 5000);
+                    waitsFor(function() {
+                        return !!config.siteToken;
+                    }, 'Get siteToken', 5000);
+                }
             }
         }
     };

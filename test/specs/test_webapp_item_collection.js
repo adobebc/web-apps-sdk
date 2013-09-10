8b930f75b5a94e762e0578315b821c085ffc38e0
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-09-10 12:00:11 +0300

    I overwrite parse method in order to correctly extract items returned by our APIs.

--- test/specs/test_webapp_item_collection.js
+++ test/specs/test_webapp_item_collection.js
@@ -7,7 +7,7 @@ describe("Unit tests for web app item collection.", function() {
 		expect(itemCollection.model).toBe(BCAPI.Models.WebApp.Item);
 	});
 	
-	it("Test web app item endpoint ok.", function() {
+	it("Test web app item collection endpoint ok.", function() {
 		var webappName = "Sample webapp",
 			rootUrl = "http://test.localhost.com/",
 			expectedUrl = rootUrl + "api/v2/admin/sites/current/webapps/Sample webapp/items",
@@ -17,4 +17,48 @@ describe("Unit tests for web app item collection.", function() {
 		
 		expect(item.url()).toBe(expectedUrl);
 	});
+	
+	/*it("Test web app item collection fetch ok.", function() {
+		var webappName = "Sample webapp",
+			rootUrl = "http://test.localhost.com/",
+			expectedUrl = rootUrl + "api/v2/admin/sites/current/webapps/" + webappName + "/items",
+			item = new BCAPI.Models.WebApp.ItemCollection(webappName),
+			siteToken = "12345xaz",
+			successCalled = false,
+			expectedItems = {"name": "Item 1"}, {"name": "Item 2"}];
+		
+		BCAPI.Mocks.Helper.Site(undefined, siteToken, rootUrl);
+			
+		function successHandler(collection, xhr, options) {
+			expect(options.testKey).toBe("123");
+			
+			var idx = 0;
+			
+			_.each(collection.models, function(model) {
+				expect(model.name).toBe(expectedItems[idx].name);
+				expect(model.get("webapp").get("name")).toBe(webappName);
+				
+				idx++;
+			});
+			
+			successCalled = true;
+		}
+		
+		spyOn($, "ajax").andCallFake(function(request) {
+			expect(request.url.substring(0, expectedUrl.length)).toBe(expectedUrl);
+			expect(request.type).toBe("GET");
+			expect(request.headers.Authorization).toBe(siteToken);
+			
+			request.success(expectedItems);
+		});
+		
+		runs(function() {
+			item.fetch({success: successHandler,
+						testKey: "123"});
+		});
+		
+		waitsFor(function() {
+			return successCalled;
+		}, "Success handler not called.", 50);
+	});*/
 });

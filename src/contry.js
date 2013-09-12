16d3dd36dc50bc733f0a5823dd33f86d974cd351
transcedentalia <transcedentalia@gmail.com>
2013-09-12 18:25:14 +0300

    country model/collection added

---
+++ src/contry.js
@@ -0,0 +1,39 @@
+(function($) {
+    "use strict";
+
+    BCAPI.Models.Country = BCAPI.Models.Model.extend({
+        defaults: {
+            countryCode: "",
+            displayName: ""
+        },
+        /**
+         * This method returns the correct endpoint for the web app countries.
+         *
+         * @method
+         * @instance
+         * @memberOf BCAPI.Models.Country
+         */
+        endpoint: function() {
+            return "/api/v2/admin/system/countries"
+        }
+    });
+
+    /**
+     * This class provides a collection of the countries available in BC. For more information regarding how to interact
+     * with the countries {@link BCAPI.Models.Country}.
+     *
+     * @name CountryCollection
+     * @class
+     * @constructor
+     * @memberOf BCAPI.Models
+     * @example
+     * var itemCollection = new BCAPI.Models.CountryCollection();
+     */
+    BCAPI.Models.CountryCollection = BCAPI.Models.Collection.extend({
+        constructor: function(webappName, models, options) {
+            BCAPI.Models.Collection.call(this, models, options);
+        },
+        model: BCAPI.Models.Country
+    });
+
+})(jQuery);

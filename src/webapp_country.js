419c7a8b80e0bc28eee0dcd2545dcb498c840568
transcedentalia <transcedentalia@gmail.com>
2013-09-12 18:25:14 +0300

    country model/collection added

---
+++ src/webapp_country.js
@@ -0,0 +1,61 @@
+(function($) {
+    "use strict";
+
+    /**
+     * This class provides an array of countries available for a certain webapp. In order to use this collection you must provide
+     * a webapp name. For more information regarding how to interact with the countries of a web app read {@link BCAPI.Models.WebApp.Country}.
+     *
+     * @name Country
+     * @class
+     * @constructor
+     * @memberOf BCAPI.Models.WebApp
+     * @example
+     * var countries = new BCAPI.Models.WebApp.Country("Sample webapp");
+     */
+    BCAPI.Models.WebApp.Country = BCAPI.Models.Model.extend({
+        constructor: function(webappName, attributes, options) {
+            BCAPI.Models.Model.call(this, attributes, options);
+            this._webappName = webappName;
+        },
+        /**
+         * This method returns the correct endpoint
+         * for the webapp countries
+         *
+         * @method
+         * @instance
+         * @memberOf BCAPI.Models.Country
+         */
+        endpoint: function() {
+            var url = ["/api/v2/admin/sites/current/webapps/"];
+            url.push(this._webappName);
+            url.push("/countries");
+            return url.join("");
+        },
+        /**
+         * This method returns the data to be json-ified when saving
+         * The API only recieves an array of strings, so we have to extract it
+         * from the items field.
+         *
+         * @method
+         * @instance
+         * @memberOf BCAPI.Models.WebApp.Country
+         */
+        toJSON: function(){
+            return this.get('items');
+        },
+
+        /**
+         * This method performs the save to the server
+         * It is overwritten here to always force a PUT operation on the endpoint
+         *
+         * @method
+         * @instance
+         * @memberOf BCAPI.Models.WebApp.Country
+         */
+        save: function(options) {
+            options = options || {};
+            options.type = "PUT";
+            return  BCAPI.Models.Model.prototype.save.call(this, options);
+        }
+    });
+})(jQuery);
---
+++ test/specs/test_webapp_country.js
@@ -0,0 +1,54 @@
+describe("Unit tests for webapp country.", function() {
+    it("Test webapp country instantiated correctly.", function() {
+        var webappName = "Sample webapp",
+            countryCollection = new BCAPI.Models.WebApp.Country(webappName);
+
+        expect(countryCollection._webappName).toBe(webappName);
+    });
+
+    it("Test webapp country endpoint ok.", function() {
+        var webappName = "Sample webapp",
+            rootUrl = "http://test.localhost.com",
+            expectedBeginUrl = rootUrl + "/api/v2/admin/sites/current/webapps/Sample webapp/countries",
+            countryCollection = new BCAPI.Models.WebApp.Country(webappName);
+
+        BCAPI.Mocks.Helper.Site(undefined, undefined, rootUrl);
+
+        expect(countryCollection.url().substring(0, expectedBeginUrl.length)).toBe(expectedBeginUrl);
+    });
+
+    it("Test toJSON override returns the array alone", function() {
+        var data = ['US','UK','RO'];
+        var countries = new BCAPI.Models.WebApp.Country("Sample App", 1, {items: ['US','UK','RO']})
+        expect(countries.toJSON() instanceof Array).toBe(true);
+        expect(countries.toJSON().toString()).toBe(data.toString());
+    });
+
+    it("Test save calls a PUT method with the inner array", function() {
+        var data = ['US','UK','RO'];
+        var countries = new BCAPI.Models.WebApp.Country("Sample App", 1, {items: ['US','UK','RO']})
+        var ajaxCalled = false;
+
+        spyOn($, "ajax").andCallFake(function(request) {
+            expect(request.type).toBe("PUT");
+            expect(request.dataType).toBe("text");
+            expect(request.data).toBe(JSON.stringify(data))
+            ajaxCalled = true;
+        });
+
+        runs(function() {
+            countries.save();
+        });
+
+        waitsFor(function() {
+            return ajaxCalled;
+        }, "Not PUT method", 50);
+    });
+
+    if("Test default items is empty", function() {
+        var countries = new BCAPI.Models.WebApp.Country("sample", 1);
+        expect(countries.get('items') instanceof Array).toBe(true);
+        expect(countries.get('items').length).toBe(0);
+    });
+
+});

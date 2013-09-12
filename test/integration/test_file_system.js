10c37cfa063d18a83ad8ea4c83926a38a5f6d12b
Marius-Andrei Danila <mdanila@adobe.com>
2013-09-12 12:08:16 +0300

    Fixes to file system sdk

--- test/integration/test_file_system.js
+++ test/integration/test_file_system.js
@@ -1,163 +1,171 @@
 describe('BCAPI.Models.FileSystem.File', function() {
-	'use strict';
-	
-	var BcFile = BCAPI.Models.FileSystem.File;
-	var BcFolder = BCAPI.Models.FileSystem.Folder;
+    'use strict';
+    
+    var BcFile = BCAPI.Models.FileSystem.File;
+    var BcFolder = BCAPI.Models.FileSystem.Folder;
 
-	beforeEach(function() {
+    beforeEach(function() {
         BCAPI.Helper.Test.runTestServer();
+        this.addMatchers({
+            toBeLaterThan: function(date, errorMargin) {
+                errorMargin = errorMargin || 0;
+                var actual = this.actual.getTime();
+                var compared = date.getTime();
+                return actual >= (compared - errorMargin);
+            }
+        });
     });
 
-	it('should allow to create a new file by uploading content', function() {
-		var file = genFile();
-		var content = randomString();
-		var testStartTime = new Date();
-		expect(file.get('type')).toBe('file');
-		promiseScenario({
-			'promise': function() {
-				return file.upload(content).then(function() { return file.fetch(); });
-			},
-			'complete': function() {
-				expect(file.get('lastModified')).toBeDefined();
-				expect(file.get('size')).toBeDefined();
-				expect(file.get('lastModified').getTime()).not.toBeLessThan(testStartTime.getTime());
-			}
-		});
-	});
+    it('should allow to create a new file by uploading content', function() {
+        var file = genFile();
+        var content = randomString();
+        var testStartTime = new Date();
+        expect(file.get('type')).toBe('file');
+        promiseScenario({
+            'promise': function() {
+                return file.upload(content).then(function() { return file.fetch(); });
+            },
+            'complete': function() {
+                expect(file.get('lastModified')).toBeDefined();
+                expect(file.get('size')).toBeDefined();
+                expect(file.get('lastModified')).toBeLaterThan(testStartTime, 1000);
+            }
+        });
+    });
 
-	it('should download the same content that was uploaded', function() {
-		var file = genFile();
-		var content = randomString();
-		promiseScenario({
-			'promise': function() {
-				return file.upload(content).then(function() {
-					return file.download();
-				});
-			},
+    it('should download the same content that was uploaded', function() {
+        var file = genFile();
+        var content = randomString();
+        promiseScenario({
+            'promise': function() {
+                return file.upload(content).then(function() {
+                    return file.download();
+                });
+            },
 
-			'complete': function(data) {
-				expect(data).toBe(content);
-			}
-		});
-	});
+            'complete': function(data) {
+                expect(data).toBe(content);
+            }
+        });
+    });
 
-	xit('should retrieve file data correctly', function() {
-		var file = genFile();
+    xit('should retrieve file data correctly', function() {
+        var file = genFile();
 
-		promiseScenario({
-			'promise': function() {
-				var fileUploaded = file.save();
-				var dataRetrieved = fileUploaded.then(function() {
-					var newFile = new BcFile(file.folder(), file.get('name'));
-					return newFile.fetchData().then(function() {
-						return newFile;
-					});
-				});
-			},
-			'complete': function(newFile) {
-				expect(newFile.get('data').toBe(fileContent));
-			}
-		});
-	});
+        promiseScenario({
+            'promise': function() {
+                var fileUploaded = file.save();
+                var dataRetrieved = fileUploaded.then(function() {
+                    var newFile = new BcFile(file.folder(), file.get('name'));
+                    return newFile.fetchData().then(function() {
+                        return newFile;
+                    });
+                });
+            },
+            'complete': function(newFile) {
+                expect(newFile.get('data').toBe(fileContent));
+            }
+        });
+    });
 
-	it('should delete files', function() {
-		var file = genFile();
-		promiseScenario({
-			'promise': function() {
-				return file.upload(randomString()).then(function() {
-					return file.destroy().then(function() {
-						return file.fetch().then(function() {
-							return true;
-						}, function() {
-							console.log('False branch');
-							return false;
-						});
-					});
-				});
-			},
-			'complete': function(wasRetrieved) {
-				expect(wasRetrieved).toBe(false);
-			}
-		});
-	});
+    it('should delete files', function() {
+        var file = genFile();
+        promiseScenario({
+            'promise': function() {
+                return file.upload(randomString()).then(function() {
+                    return file.destroy().then(function() {
+                        return file.fetch().then(function() {
+                            return true;
+                        }, function() {
+                            var p = $.Deferred();
+                            p.resolve(false);
+                            return p;
+                        });
+                    });
+                });
+            },
+            'complete': function(wasRetrieved) {
+                expect(wasRetrieved).toBe(false);
+            }
+        });
+    });
 
-	it('should handle re-upload properly', function() {
-		var file = genFile();
-		var initialData = randomString();
-		var newData = randomString();
-		var creationTimestamp;
-		promiseScenario({
-			'promise': function() {
-				return file.uploadAndFetch(initialData)
-					.done(function() {
-						creationTimestamp = file.get('lastModified');
-					})
-					.then(function() {
-						return file.uploadAndFetch(newData).then(function() {
-							return file.download();	
-						});
-					});
-			},
-			'complete': function(downloadedData) {
-				expect(downloadedData).toBe(newData);
-				expect(file.get('data')).toBe(newData);
-				expect(file.get('lastModified').getTime()).toBeGreaterThan(creationTimestamp.getTime());
-			}
-		});
-	});
+    it('should handle re-upload properly', function() {
+        var file = genFile();
+        var initialData = randomString();
+        var newData = randomString();
+        var creationTimestamp;
+        promiseScenario({
+            'promise': function() {
+                return file.uploadAndFetch(initialData)
+                    .done(function() {
+                        creationTimestamp = file.get('lastModified');
+                    })
+                    .then(function() {
+                        return file.uploadAndFetch(newData).then(function() {
+                            return file.download(); 
+                        });
+                    });
+            },
+            'complete': function(downloadedData) {
+                expect(downloadedData).toBe(newData);
+                expect(file.get('lastModified')).toBeLaterThan(creationTimestamp);
+            }
+        });
+    });
 
-	// Utility functions
-	
-	function randChar() {
-		var possibleChars = 'abcdefghijklmnopqrstuvwxyz';
-		var index = Math.floor(Math.random() * possibleChars.length);
-		return possibleChars[index];
-	}
+    // Utility functions
+    
+    function randChar() {
+        var possibleChars = 'abcdefghijklmnopqrstuvwxyz';
+        var index = Math.floor(Math.random() * possibleChars.length);
+        return possibleChars[index];
+    }
 
-	function randomString(size) {
-		size = size || 32;
-		return _.times(size, randChar).join('');
-	}
+    function randomString(size) {
+        size = size || 32;
+        return _.times(size, randChar).join('');
+    }
 
-	function genFileName() {
-		return 'generated_' + randomString(12);
-	}
+    function genFileName() {
+        return 'generated_' + randomString(12);
+    }
 
-	function promiseScenario(scenario) {
-		var promiseFun = scenario.promise,
-			promiseCompletion = scenario.complete,
-			message = scenario.message || 'Promise in testing scenario',
-			finished = false,
-			success,
-			result;
-		runs(function() {
-			var p = promiseFun();
-			p.done(function(x) {
-				result = x;
-				finished = true;
-				success = true;
-			}).fail(function(jqXHR, textStatus, errorThrown) {
-				console.log('Promise failed. Error status was: ' + textStatus + '. Error thrown was: ' + errorThrown);
-				finished = true;
-				success = false;
-				console.log('Call has failed');
-			});
-		});
-		waitsFor(function() { return finished; }, message, 2000);
-		runs(function() {
-			expect(success).toBeTruthy('Promise completion should be successful');
-			if (success) {
-				promiseCompletion(result);	
-			}
-		});
-	}
+    function promiseScenario(scenario) {
+        var promiseFun = scenario.promise,
+            promiseCompletion = scenario.complete,
+            message = scenario.message || 'Promise in testing scenario',
+            finished = false,
+            success,
+            result;
+        runs(function() {
+            var p = promiseFun();
+            p.done(function(x) {
+                result = x;
+                finished = true;
+                success = true;
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                console.log('Promise failed. Error status was: ' + textStatus + '. Error thrown was: ' + errorThrown);
+                finished = true;
+                success = false;
+                console.log('Call has failed');
+            });
+        });
+        waitsFor(function() { return finished; }, message, 2000);
+        runs(function() {
+            expect(success).toBeTruthy('Promise completion should be successful');
+            if (success) {
+                promiseCompletion(result);  
+            }
+        });
+    }
 
-	function genFile(directory) {
-		directory = directory || BcFolder.Root;
-		var fileName = genFileName();
-		return directory.file({
-			'name': fileName
-		});
-	}
+    function genFile(directory) {
+        directory = directory || BcFolder.Root;
+        var fileName = genFileName();
+        return directory.file({
+            'name': fileName
+        });
+    }
 
 });

939640a7438ac1b28c369c700dbd2f914e6952af
Bogdan Ripa <bripa@adobe.com>
2013-09-12 16:37:21 +0300

    Initial commit This is the back-end of the application Front-end includin layouts will ve included soon The file system "own" lib will be replaced with BCAPI's

---
+++ _admin/BC/gallery/WAHelper.js
@@ -0,0 +1,91 @@
+// JavaScript Document
+if (typeof wahelper == 'undefined') {
+	var wahelper = {};
+}
+
+wahelper.fixWeightInconsistancies = function(data) {
+    wahelper.maxWeight = 1;
+    
+    for (var i=0;i<data.models.length-1;i++) {
+
+        if (data.models[i].attributes.weight == 'null') {
+
+            data.models[i].set({
+                weight: newWeights[itemId]
+            });
+            data.models[i].save({});
+            
+            console.log("Fixing webapp null weighting for #" + data.models[i].id + ". Fixing to " + data.models[i].attributes.weight);
+        }
+
+        if (data.models[i].attributes.weight == data.models[i+1].attributes.weight) {
+
+            data.models[i+1].set({
+                weight: data.models[i].attributes.weight+1
+            });
+            data.models[i+1].save({});
+            
+            console.log("Fixing webapp duplicate weighting for #" + data.models[i+1].id + ". Fixing to " + data.models[i+1].attributes.weight);
+        }
+
+        wahelper.maxWeight = data.models[i+1].attributes.weight+1;
+    }
+    return data;
+}
+
+// update weightings
+wahelper.updateWeightings = {};
+
+wahelper.updateWeightings.start = function(event, ui) {
+	wahelper.updateWeightings.oldIndex = ui.item.index();
+};
+
+wahelper.updateWeightings.update = function(event, ui, waName, callback) {
+    var items = ui.item.parent().children();
+
+    var newOrders = {};
+    var newIndex = ui.item.index();
+    var from, to, dir;
+    
+    if (newIndex < wahelper.updateWeightings.oldIndex) {
+        from = newIndex;
+        to = wahelper.updateWeightings.oldIndex;
+        dir = 1;
+    } else {
+        from = wahelper.updateWeightings.oldIndex+1;
+        to = newIndex+1;
+        dir = -1;
+    }
+
+    for (var i=from;i<to;i++) {
+        newOrders[$(items[i]).attr('id').replace(/[^\d]/g, '')] = $(items[i+dir]).data('weight');
+    }
+
+    newOrders[$(items[wahelper.updateWeightings.oldIndex]).attr('id').replace(/[^\d]/g, '')] = $(items[newIndex]).data('weight');
+
+    
+    if (newIndex < wahelper.updateWeightings.oldIndex) {
+        from = newIndex;
+        to = wahelper.updateWeightings.oldIndex;
+    } else {
+        from = wahelper.updateWeightings.oldIndex;
+        to = newIndex;
+    }
+
+    for (var i=from;i<=to;i++) {
+		$(items[i]).data('weight', newOrders[$(items[i]).attr('id').replace(/[^\d]/g, '')]);
+    }
+
+    wahelper.updateWeightings.doUpdate(waName, newOrders, callback);
+};
+
+wahelper.updateWeightings.doUpdate = function(waName, newWeights, callback) {
+    for (var itemId in newWeights) {
+        var itemToUpdate = wadata.get(itemId);
+        itemToUpdate.set({
+            "weight": newWeights[itemId]
+       	});
+        itemToUpdate.save({});
+    }
+    callback();
+};
---
+++ _admin/BC/gallery/gallery.js
@@ -0,0 +1,204 @@
+// JavaScript Document
+
+var IMAGES_PATH = '/images/gallery/';
+var WEBAPP_NAME = 'Gallery';
+
+$(function() {
+
+    // update the page title
+    parent.document.title = document.title;
+    
+    loadImages();
+    $('#newItem').change(addImages);
+    $('#trashIcon').click(initDeleteImage);
+    $('#newItemButton').click(function() {
+        $('#newItem').click();
+    });
+});
+
+// begin image loading functions
+function loadImages() {
+    wadata = {};
+    $('.allimages').html('Loading...');
+    
+    // loads all the webapp items
+    var items = new BCAPI.Models.WebApp.ItemCollection(WEBAPP_NAME);
+    items.fetch({
+        order: "weight",
+        limit: 1000, // get all items
+        success: loadImagesCB,
+        error: createWebApp
+    });
+}
+
+// callback when the list of webapp items was retrieved
+function loadImagesCB(data) {
+    
+    if (data.models.length == 0) {
+        // if no items found
+    	$('.allimages').html('Please add at least one image.');
+        return;
+    }
+    
+    // fix weight-related inconsistencies
+    wadata = wahelper.fixWeightInconsistancies(data);
+
+    // call the "doneLoading" function when all individual items have been fetched
+    _.doneLoading = _.after(wadata.models.length, doneLoading);
+    var i = 0;
+    
+    _.each(wadata.models, function(item) {
+        var localI = i++;
+
+        // get the fill items that includes all custom fields
+        var fullItem = new BCAPI.Models.WebApp.Item(WEBAPP_NAME);
+        fullItem.id = item.id;
+
+        fullItem.fetch({
+            success: function(item) {
+                // update the local model (that did not include the custom fields) with the one with the custom fields
+                wadata.models[localI] = item;
+                _.doneLoading();
+            }
+        });
+    });
+    
+}
+
+// callback when all individual items have been loaded
+function doneLoading() {
+    // use Liquid to generate the list of images
+    var html = $('#allimagesscript').html();
+    html = Liquid.Template.parse(html).render(wadata);
+    $('.allimages').html(html);
+
+    // use jQuery sortable to handle drag and drop between items
+    $('.allimages').sortable({
+        start: wahelper.updateWeightings.start,
+        update: function(event, ui) {
+            wahelper.updateWeightings.update(event, ui, WEBAPP_NAME, doneSorting);
+        }
+    });
+    
+    $('.allimages li').mouseover(mouseOver).mouseout(mouseOut);
+}
+// end image loading functions
+
+// callback after re-ordering elements (API calls) is done 
+function doneSorting() {
+    $.jGrowl("Sorting updated.");
+}
+
+// on mouse over, show the trash icon
+function mouseOver() {
+    $(this).append($('#trashIcon'));
+	$('#trashIcon').show();
+}
+
+// on mouse out, hide the trash icon
+function mouseOut(){
+	$('#trashIcon').hide();
+}
+
+// begin delete image functions
+// when the user clicks the delete button
+function initDeleteImage() {
+    if (confirm("Are you sure you want to delete this image?")) {
+        // this is the ID of the webapp item that was clicked
+        var id = $(this).parent().attr('id').replace(/image_/, '');
+        
+        var imgSrc = $(this).parent().find('img').attr('src').replace(/\?.*$/, '');
+        
+		$('#trashIcon').hide();
+	    $('body').append($('#trashIcon'));
+        $('.allimages').html('');
+        
+        // TODO: replace with BCAPI call
+        fs.delete(imgSrc);
+        
+        var itemToDelete = new BCAPI.Models.WebApp.Item(WEBAPP_NAME, {id: id});
+        itemToDelete.destroy({
+            success: imageDeleted
+        });        
+    }
+}
+
+// callback after the image was deleted
+function imageDeleted() {
+    $.jGrowl("Image deleted.");
+    loadImages();
+}
+// end delete image functions
+
+
+// begin add image functions
+// called when the user selected some images to upload
+function addImages(e) {
+    $.jGrowl("Uploading images...");
+    
+    // callback is called when after images upload and items created
+    _.imagesUploaded = _.after($('#newItem')[0].files.length*2, imagesUploaded);
+    
+    _.each($('#newItem')[0].files, function(file) {
+        addImage(file);
+    });
+}
+
+// upload individual image and create the webapp item
+function addImage(file) {
+
+    var path = IMAGES_PATH + file.name;
+
+    // call the FS API to create the file
+    fs.upload(file, path, _.imagesUploaded);
+    
+    // call the WA Create API to create the WA Item
+    var newItem = new BCAPI.Models.WebApp.Item(WEBAPP_NAME, {
+        name: file.name,
+        weight: wahelper.maxWeight,
+        fields: {
+            Image: path
+        }
+    });
+    
+    var response = newItem.save({
+            success: _.imagesUploaded
+    });    
+}
+
+// callback when all images have been uploaded and all webapp items created
+function imagesUploaded() {
+    $('body').append($('#trashIcon'));
+    $.jGrowl("Images uploaded.");
+    resetFormElement($('#newItem'));
+    loadImages();
+}
+
+function resetFormElement(e) {
+  e.wrap('<form>').closest('form').get(0).reset();
+  e.unwrap();
+}
+
+// end add image functions
+
+// init functions. Creates the webapp if not created
+function createWebApp() {
+    var app = new BCAPI.Models.WebApp.App({
+            name: WEBAPP_NAME
+    });
+    
+    // create the web app
+    app.save({
+        success: function() {
+            var customField = new BCAPI.Models.WebApp.CustomField(WEBAPP_NAME, {
+                name: "Image",
+                type: "Image"
+            });
+            
+            // create the webapp "Image" custom fields
+            customField.save({
+                success: loadImages
+            });
+        }
+    });
+}

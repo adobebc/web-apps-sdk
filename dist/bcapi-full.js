bba382299df633f9f8bbc77104f2479d09126c45
TC-Jenkins <TC-Jenkins@adobe.com>
2013-09-12 11:59:25 +0300

    Latest version of bcapi distribution.

--- dist/bcapi-full.js
+++ dist/bcapi-full.js
@@ -3324,7 +3324,7 @@
         fetch: function(options) {
         	options = options || {};
 
-        	oldSuccess = options.success;
+        	var oldSuccess = options.success;
         	
         	options.success = function(collection, xhr, options) {
         		var currFetchedFields = 0;

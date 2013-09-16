2a576956f1242f83a920499eba1674e7c3fbf474
Cristian Ivascu <civascu@adobe.com>
2013-09-16 11:09:08 +0300

    Removed development related file

--- Meet The Team/team/_admin/assets/overrides.js
+++
@@ -1,8 +0,0 @@
-BCAPI.Helper.Site.getRootUrl = function() {
-    return "https://secure-imc.bc.corp.adobe.com/";
-}
-
-BCAPI.Helper.Site.getSiteToken = function() {
-     return "c6d8e445dd8f49149def6f2b0be519fa97c536f73cfd4087a6330e44f43e5d2b";
-}
-

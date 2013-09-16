e6e7651fa9a3fe53ed8d3e8c5d4059b6649fc731
Radu Viorel Cosnita <cosnita@adobe.com>
2013-09-16 18:34:20 +0300

    We don't support relation for now. In the future back will support relation fetching.

--- test/specs/test_webapp_collection.js
+++ test/specs/test_webapp_collection.js
@@ -1,20 +1,4 @@
-describe("Unit tests for webapp collection.", function() {
-	var oldFieldCollection = undefined,
-		oldMockFieldsFetch = undefined,
-		expectedFields = [{"id": 1, "name": "Field 1", "type": "DataSource", "listItems": [1, 2, 3],
-						   "required": false, "order": 5}, 
-						  {"id": 1500, "name": "Field 2", "type": "String", "required": true}];
-	
-	beforeEach(function() {
-		oldFieldCollection = BCAPI.Models.WebApp.CustomFieldCollection;
-		oldMockFieldsFetch = MockCustomFieldCollection.prototype.fetch;
-	});
-	
-	afterEach(function() {
-		BCAPI.Models.WebApp.CustomFieldCollection = oldFieldCollection;
-		MockCustomFieldCollection.prototype.fetch = oldMockFieldsFetch;
-	});
-	
+describe("Unit tests for webapp collection.", function() {	
 	it("Check app collection instantiated ok.", function() {
 		var appCollection = new BCAPI.Models.WebApp.AppCollection();
 		
@@ -43,34 +27,16 @@ describe("Unit tests for webapp collection.", function() {
 			successCalled = false;
 		
 		BCAPI.Mocks.Helper.Site(undefined, siteToken, rootUrl);
-		BCAPI.Models.WebApp.CustomFieldCollection = MockCustomFieldCollection;		
 		
 		function successHandler(collection, xhr, options) {
 			var idx = 0;
 			
 			expect(options.testKey).toBe(1234);
-			expect(options.fetchFields).toBe(true);
 			
 			collection.each(function(model) {
 				expect(model.get("id"), expectedApps.items[idx].id);
 				expect(model.get("name"), expectedApps.items[idx].name);
 				expect(model.get("slug"), expectedApps.items[idx].slug);
-				
-				fields = model.get("fields");
-				expect(fields.length, expectedFields.length);
-				
-				var fieldIdx = 0;
-				fields.each(function(field) {
-					expect(field.get("webappName")).toBe(model.get("name"));
-					expect(field.get("id")).toBe(expectedFields[fieldIdx].id);
-					expect(field.get("name")).toBe(expectedFields[fieldIdx].name);
-					expect(field.get("type")).toBe(expectedFields[fieldIdx].type);
-					expect(field.get("listItems")).toBe(expectedFields[fieldIdx].listItems);
-					expect(field.get("required")).toBe(expectedFields[fieldIdx].required);
-					expect(field.get("order")).toBe(expectedFields[fieldIdx].order);
-					
-					fieldIdx++;
-				});
 				
 				idx++;
 			});
@@ -89,7 +55,6 @@ describe("Unit tests for webapp collection.", function() {
 		runs(function() {
 			appCollection.fetch({
 				testKey: 1234,
-				fetchFields: true,
 				success: successHandler
 			});
 		});
@@ -98,97 +63,4 @@ describe("Unit tests for webapp collection.", function() {
 			return successCalled;
 		}, "Success handler not called.", 50);
 	});
-	
-	it("Check app collection fails on eagerly fetching custom fields.", function() {
-		var appCollection = new BCAPI.Models.WebApp.AppCollection(),
-			siteToken = "12345xxaz",
-			rootUrl = "http://test.localhost.com",
-			expectedUrl = rootUrl + "/api/v2/admin/sites/current/webapps",
-			expectedApps = {"items": [{"id":346, "name":"BC Friends", "slug":"bc-friends"},
-			                          {"id":356, "name":"BC Help", "slug":"bc-help"}]},
-			itemErrorCalled = 0,
-			errorCalled = 0;
-
-		BCAPI.Mocks.Helper.Site(undefined, siteToken, rootUrl);
-		BCAPI.Models.WebApp.CustomFieldCollection = MockCustomFieldCollection;		
-		
-		MockCustomFieldCollection.prototype.fetch = function(options) {
-			var xhr = {responseText: "Failed call."};
-			
-			options.error(undefined, xhr, options);
-		};		
-		
-		function errorHandler(collection, response, options) {
-			expect(collection).toBe(appCollection);
-			expect(options.testKey).toBe(1234);
-			expect(options.fetchFields).toBe(true);
-			
-			errorCalled++;
-		}
-		
-		function itemErrorHandler(webapp, relationId, response, options) {
-			expect(typeof webapp).toBe(BCAPI.Models.WebApp.App);
-			expect(relationId).toBe("fields");
-			expect(options.testKey).toBe(1234);
-			expect(options.fetchFields).toBe(true);
-			
-			itemErrorCalled++;
-		}
-		
-		spyOn($, "ajax").andCallFake(function(request) {
-			expect(request.url.substring(0, expectedUrl.length)).toBe(expectedUrl);
-			expect(request.type).toBe("GET");
-			expect(request.headers.Authorization).toBe(siteToken);
-
-			request.success(expectedApps);
-		});
-				
-		runs(function() {
-			appCollection.fetch({
-				testKey: 1234,
-				fetchFields: true,
-				itemError: itemErrorHandler,
-				error: errorHandler
-			});
-		});
-
-		waitsFor(function() {
-			return itemErrorCalled > 0;
-		}, "Error handler not called.", 50);
-		
-		runs(function() {
-			expect(itemErrorCalled).toBe(2);
-			expect(errorCalled).toBe(1);
-		});
-	});
-	
-	/**
-	 * This is a simple mock object for custom field collection.
-	 */
-	function MockCustomFieldCollection(webappName, attributes) {
-		this._webappName = webappName;
-		this.attributes = attributes;
-		this.length = expectedFields.length;
-	}
-	
-	MockCustomFieldCollection.prototype.get = function(attr) {
-		return this.attributes[attr];
-	};
-	
-	MockCustomFieldCollection.prototype.fetch = function(options) {
-		var fields = [],
-			self = this;
-		
-		_.each(expectedFields, function(field) {
-			fields.push(new BCAPI.Models.WebApp.CustomField(self._webappName, field));
-		});
-		
-		this.each = function(callback) {
-			_.each(fields, function(field) {
-				callback(field);
-			});
-		};
-		
-		options.success(this, undefined, options);
-	};
 });

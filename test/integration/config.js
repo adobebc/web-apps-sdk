07bf3917c06a25c6e7bfae38af4f4b0e96222745
Radu Viorel Cosnita <cosnita@adobe.com>
2013-09-17 15:34:23 +0300

    I added integration tests for webapp country.

--- test/integration/config.js
+++ test/integration/config.js
@@ -4,8 +4,8 @@
 		siteId: 38604,	// bcapi-integration
 		username: 'bcadmin@bc1.local', //'bcapi-integration@mailinator.com',
 		password: 'password9', // '123456'
-		genericToken: "e06e4169df114574bb091ec209674903e6edc48409304d708747e53935d37a8b",
-		siteToken: "1f1aada4413e4ecf86309cbf704f0a9dcc3618589bd345d7b0bdf9ac184a23f7",
+		genericToken: "",
+		siteToken: "",
 		tokenRequestTimeout: 60 * 1000
 	};
 })();

46895adb8eb2f74fc86f9e5a0e08294180b3861e
Radu Viorel Cosnita <cosnita@adobe.com>
2013-09-17 14:10:47 +0300

    I added integration tests for country collection listing.

---
+++ test/integration/test_country.js
@@ -0,0 +1,35 @@
+describe("Check country model collection integration.", function() {
+    beforeEach(function() {
+        BCAPI.Helper.Test.runTestServer();
+    });
+    
+    it("Check country listing works as expected.", function() {
+    	var countryCollection = new BCAPI.Models.CountryCollection(),
+    		fetchedCollection = undefined,
+    		foundCountries = {};
+    	
+    	runs(function() {
+    		countryCollection.fetch({
+    			success: function(countries) {
+    				fetchedCollection = countries;
+    			}
+    		});    		
+    	});
+    	
+    	waitsFor(function() {
+    		return fetchedCollection;
+    	}, "List of countries not fetched.", 2000);
+    	
+    	runs(function() {
+			expect(fetchedCollection.length).toBe(250);
+    		
+    		fetchedCollection.each(function(country) {
+    			foundCountries[country.countryCode] = country;
+    		});
+    		
+    		expect(foundCountries.AU).toBe("Australia");
+    		expect(foundCountries.RO).toBe("Romania");
+    		expect(foundCountries.US).toBe("United States");
+    	});
+    });
+});

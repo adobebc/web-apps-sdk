350049eb725932fc58c72a073fe0aae22c6f559b
Radu Viorel Cosnita <cosnita@adobe.com>
2013-09-18 11:27:32 +0300

    I removed hardcoded tokens used in integration tests.

--- test/integration/config.js
+++ test/integration/config.js
@@ -4,8 +4,8 @@
 		siteId: 38604,	// bcapi-integration
 		username: 'bcadmin@bc1.local', //'bcapi-integration@mailinator.com',
 		password: 'password9', // '123456'
-		genericToken: "dbb34ec283314b4ba0fc428834142c152c65b78e82974308a47cff4f29f41499",
-		siteToken: "e3d0f65538a444f7b9a0b12ca0370d7da7832cb40d19416da55e27eec9b7da42",
+		genericToken: "",
+		siteToken: "",
 		tokenRequestTimeout: 60 * 1000
 	};
 })();

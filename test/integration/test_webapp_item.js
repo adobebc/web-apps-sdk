20cfd2e09c3f1387da749a1846ace61e8e98d170
Marius-Andrei Danila <mdanila@adobe.com>
2013-09-18 16:40:01 +0300

    more integration tests

--- test/integration/test_webapp_item.js
+++ test/integration/test_webapp_item.js
@@ -3,6 +3,7 @@ describe('BCAPI.Models.WebApp', function() {
     var promiseScenario = BCAPI.Helper.Test.PromiseUtils.promiseScenario;
     var promiseFlag = BCAPI.Helper.Test.PromiseUtils.promiseFlag;
     var Item = BCAPI.Models.WebApp.Item;
+    var ItemCollection = BCAPI.Models.WebApp.ItemCollection;
 
     var WEBAPP_NAME = 'WebApp_ItemIntegrationTest';
     var webApp = defaultWebApp();
@@ -95,12 +96,91 @@ describe('BCAPI.Models.WebApp', function() {
         
     });
 
+    describe('BCAPI.Models.WebApp.ItemCollection', function() {
+
+        it('should perform the correct queries', function() {
+            var count = 20;
+            promiseScenario({
+                'promise': function() {
+                    return createMultiple(count, function(index) {
+                        var item = newItem('col-item-' + (index <= 9 ? '0'+index : index));
+                        item.set('weight', index);
+                        return item;
+                    }).then(function() {
+                        var col = createCollection();
+                        // selecting items [col-index-17 .. col-index-11]
+                        return col.fetch({
+                            'limit': 7,
+                            'skip': 2,
+                            'where': {
+                                'weight': {
+                                    '$gt': 9
+                                }
+                            },
+                            'order': '-name'
+                        }).then(function() {
+                            return col;
+                        });
+                    });
+                },
+                'complete': function(col) {
+                    expect(col.length).toBe(7);
+                    for (var i = 0; i < 7; i++) {
+                        var item = col.at(i);
+                        var index = 17 - i;
+                        expect(item.get('name')).toBe('col-item-' + index);
+                        expect(item.get('weight')).toBe(index);
+                    }
+                }
+            });
+        });
+
+        xit('should allow creation of items', function() {
+            var col;
+            var initialItemCount = 0;
+            promiseScenario({
+                'promise': function() {
+                    return newItem('created-item-1').save()
+                        .then(function() {
+                            col = createCollection();
+                            return col.fetch();
+                        }).then(function() {
+                            initialItemCount = col.length;
+                            var p = $.Deferred();
+                            col.on('sync', function() {
+                                p.resolve(true);
+                            });
+                            col.create({
+                                'name': 'created-item-2',
+                                'description': 'some-description',
+                                'weight': 42
+                            });
+                            return p;
+                        }).then(function() {
+                            return retrieveByName('created-item-2');
+                        });
+                },
+                'complete': function(item) {
+                    expect(initialItemCount).toBe(1);
+                    expect(item.get('description')).toBe('some-description');
+                    expect(item.get('weight')).toBe(42);
+                },
+                'message': 'Creating items using collection'
+            });
+        });
+
+    });
+
     function newItem(name) {
         return new Item(webApp.get('name'), {'name': name});
     }
 
+    function createCollection() {
+        return new ItemCollection(webApp.get('name'));
+    }
+
     function retrieveByName(name, withDetails) {
-        var items = new BCAPI.Models.WebApp.ItemCollection(webApp.get('name'));
+        var items = createCollection();
         var p = items.fetch({
             'limit': 1,
             'where': {
@@ -122,5 +202,21 @@ describe('BCAPI.Models.WebApp', function() {
         } else {
             return p;
         }
+    }
+
+    function createMultiple(count, creator) {
+        var p = $.Deferred();
+        var remaining = count;
+        var decrement = function() {
+            remaining--;
+            if (!remaining) {
+                p.resolve(true);
+            }
+        };
+        for (var i = 0; i < count; i++) {
+            var item = creator(i);
+            item.save().done(decrement);
+        }
+        return p;
     }
 });

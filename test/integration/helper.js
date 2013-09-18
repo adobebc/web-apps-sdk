b246569a772df1b32abb23830820c35e0a48f031
Marius-Andrei Danila <mdanila@adobe.com>
2013-09-18 15:07:03 +0300

    Added web app item integration tests

--- test/integration/helper.js
+++ test/integration/helper.js
@@ -75,4 +75,68 @@
         }
     };
 
+
+    
+    BCAPI.Helper.Test.PromiseUtils = {
+        /**
+         * Utility function to test aynchronous workflows. The function
+         * takes a scenario object which must contain two properties: `promise`
+         * and `complete`. The promise field must be a nullary function which
+         * returns a promise. The complete field must be a function with
+         * a single argument denoting the result of the promise - if the promise
+         * was succesful.
+         * A test written with this utility consists of specifying a promise to
+         * be run (which may be constructed by chaining multiple promises together)
+         * and a `complete` part where all the assertions are made about the
+         * outcome of the promise
+         * 
+         * @param  {object} scenario the scenario object
+         */
+        promiseScenario: function(scenario) {
+            var promiseFun = scenario.promise,
+                promiseCompletion = scenario.complete,
+                message = scenario.message || 'Promise in testing scenario',
+                finished = false,
+                success,
+                result;
+            runs(function() {
+                var p = promiseFun();
+                p.done(function(x) {
+                    result = x;
+                    finished = true;
+                    success = true;
+                }).fail(function(jqXHR, textStatus, errorThrown) {
+                    console.log('Promise failed. Error status was: ' + textStatus + '. Error thrown was: ' + errorThrown);
+                    finished = true;
+                    success = false;
+                });
+            });
+            waitsFor(function() { return finished; }, 'Promise timed out for scenario: ' + message, 5000);
+            runs(function() {
+                expect(success).toBeTruthy(scenario.message);
+                if (success) {
+                    promiseCompletion(result);  
+                }
+            });
+        },
+
+        /**
+         * Takes a promise and returns another promise which resolves
+         * to a boolean value - true if the original promise succeded
+         * or false otherwise
+         * @param  {promise} promise a then-able promise
+         * @return {promise}         a promise indicated the success status of the
+         *                           original promise
+         */
+        promiseFlag: function(promise) {
+            return promise.then(function() {
+                return true;
+            }, function() {
+                var p = $.Deferred();
+                p.resolve(false);
+                return p;
+            });
+        }
+    };
+
 })(jQuery);

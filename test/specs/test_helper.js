78bcaac2a86f72ec9123d87e255fcd2e860783e7
Camelia Dobrin <cadobrin@adobe.com>
2013-10-23 16:09:32 +0300

    Fixed BCAPI.Helper.Site.getAccessToken to retrieve access_token from URL hash fragment (vs. query parameters as it was before). Updated unit tests for BCAPI.Helper.Http

--- test/specs/test_helper.js
+++ test/specs/test_helper.js
@@ -1,26 +1,25 @@
 describe("Helper.Site", function() {
 
-    it("tries to use JQuery.cookie to get tokens", function() {
+    it("Test if no $.cookie", function(){     
         spyOn($, "error");
 
         BCAPI.Helper.Site.getAccessToken();
         expect($.error).toHaveBeenCalled();
+    });
 
+    it("Test it gets token from cookie if it exists", function(){
         spyOn($, "cookie").andCallFake(function(){return true;});
 
         BCAPI.Helper.Site.getAccessToken();
         expect($.cookie).toHaveBeenCalledWith('access_token');
-
-        expect(BCAPI.Helper.Site.getSiteId()).toBe('current');
     });
-
-    it("gets token from URL parameter", function() {
-        
+ 
+    it("Test it gets token from hash parameters if it it's not in cookie", function(){
         spyOn($, "cookie").andCallFake(function(){return undefined;});
-        
+
         spyOn(BCAPI.Helper.Http, "getCurrentLocation").andCallFake(function(){
             var link = document.createElement("a");
-            link.href = "http://businesscatalyst.com?access_token=febf7b6a027";
+            link.href = "http://businesscatalyst.com#access_token=febf7b6a027";
             return link;
         });
 
@@ -29,7 +28,11 @@ describe("Helper.Site", function() {
         expect($.cookie).toHaveBeenCalledWith('access_token','febf7b6a027');
     });
 
-    it("getRootUrl()", function() {
+    it("Test getSiteId returns 'current'", function(){
+        expect(BCAPI.Helper.Site.getSiteId()).toBe('current');
+    });
+
+    it("Test getRootUrl()", function() {
         expect(BCAPI.Helper.Site.getRootUrl()).toBe("");
 
         var host = 'secured.bc-local.com';
@@ -42,22 +45,99 @@ describe("Helper.Site", function() {
 
 describe("Helper.Http", function(){
 
-    function _getMockLocation(fakeURLParams){
+    function _getNumberOfOwnProperties(obj){
+        var counter = 0;
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    function _getMockLocation(location){
         var link = document.createElement("a");
-        link.href = "http://businesscatalyst.com" + fakeURLParams;
+        link.href = location;
         return link;
     }
 
-    it("Check it ignores params without value", function(){
-        var mockLocation = _getMockLocation("?paramNoValue&paramWithValue=myValue");
-        var params = BCAPI.Helper.Http.getQueryParameters(mockLocation);
-        expect(params['paramNoValue']).toBe(undefined);
-        expect(params['paramWithValue']).toBe('myValue');
+    describe("Helper.Http.getQueryParameters", function(){
+        it("Test if location is undefined", function(){
+            var mockLocation = _getMockLocation();
+            var params = BCAPI.Helper.Http.getQueryParameters(mockLocation);
+            
+            expect(_getNumberOfOwnProperties(params)).toBe(0);
+        });
+
+        it("Test if parameter string is empty (no ?)", function(){
+            var mockLocation = _getMockLocation("http://app.businesscatalyst.com");
+            var params = BCAPI.Helper.Http.getQueryParameters(mockLocation);
+            
+            expect(_getNumberOfOwnProperties(params)).toBe(0);
+        });
+
+        it("Check it returns correct number of parameters", function (){
+            var mockLocation = _getMockLocation("http://app.businesscatalyst.com?param1=value1&param2=value2&param3=value3");
+            var params = BCAPI.Helper.Http.getQueryParameters(mockLocation);
+
+            expect(_getNumberOfOwnProperties(params)).toBe(3);
+        });
     });
 
-    it("Check it decodes param values", function(){
-        var mockLocation = _getMockLocation("?encodedParam=my%26Value");
-        var params = BCAPI.Helper.Http.getQueryParameters(mockLocation);
-        expect(params['encodedParam']).toBe('my&Value');
+    describe("Helper.Http.getHashFragments", function(){
+        it("Test if location is undefined", function(){
+            var mockLocation = _getMockLocation();
+            var params = BCAPI.Helper.Http.getHashFragments(mockLocation);
+            
+            expect(_getNumberOfOwnProperties(params)).toBe(0);
+        });
+
+        it("Test if parameter string is empty (no #)", function(){
+            var mockLocation = _getMockLocation("http://app.businesscatalyst.com");
+            var params = BCAPI.Helper.Http.getHashFragments(mockLocation);
+            
+            expect(_getNumberOfOwnProperties(params)).toBe(0);
+        });
+
+        it("Check it returns correct number of parameters", function (){
+            var mockLocation = _getMockLocation("http://app.businesscatalyst.com#param1=value1&param2=value2&param3=value3");
+            var params = BCAPI.Helper.Http.getHashFragments(mockLocation);
+
+            expect(_getNumberOfOwnProperties(params)).toBe(3);
+        });
     });
+
+    describe("Helper.Http.getDecodedParameters", function(){
+        it("Check it returns correct parameter", function (){
+            var params = BCAPI.Helper.Http.getDecodedParameters('param1=value1');
+
+            expect(_getNumberOfOwnProperties(params)).toBe(1);
+            expect(params['param1']).toBe('value1');
+        });
+
+        it("Check it works with multiple parameters", function (){
+            var params = BCAPI.Helper.Http.getDecodedParameters('param1=value1&param2=value2&param3=value3');
+
+            expect(_getNumberOfOwnProperties(params)).toBe(3);
+            expect(params['param1']).toBe('value1');
+            expect(params['param2']).toBe('value2');
+            expect(params['param3']).toBe('value3');
+        });
+
+        it("Check it ignores params without value", function(){
+            var params = BCAPI.Helper.Http.getDecodedParameters('paramNoValue&paramWithValue=myValue');
+
+            expect(_getNumberOfOwnProperties(params)).toBe(1);
+            expect(params['paramNoValue']).toBe(undefined);
+            expect(params['paramWithValue']).toBe('myValue');
+        });
+
+        it("Check it decodes param values", function(){
+            var params = BCAPI.Helper.Http.getDecodedParameters('encodedParam=my%26Value');
+
+            expect(params['encodedParam']).toBe('my&Value');
+        });
+
+    });
+
 });

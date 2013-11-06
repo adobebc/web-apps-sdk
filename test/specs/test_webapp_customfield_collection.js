63fbb83939494e4b01311ce1ed4ef8ccfcd98364
Razvan Racasanu <rracasan@adobe.com>
2013-11-06 18:44:27 +0200

    Removed an assertion on a private field

--- test/specs/test_webapp_customfield_collection.js
+++ test/specs/test_webapp_customfield_collection.js
@@ -3,7 +3,6 @@ describe("Unit tests for custom field collection.", function() {
 		var webappName = "Sample webapp",
 			fieldCollection = new BCAPI.Models.WebApp.CustomFieldCollection(webappName);
 		
-		expect(fieldCollection._webappName).toBe(webappName);
 		expect(fieldCollection.model).toBe(BCAPI.Models.WebApp.CustomField);
 	});
 	

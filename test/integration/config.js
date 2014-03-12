8d2f26b24e5fca1194886ece55b48ada8e9c5aaf
cranasta <cranasta@adobe.com>
2014-03-12 15:29:07 +0200

    updated server information

--- test/integration/config.js
+++ test/integration/config.js
@@ -23,10 +23,10 @@
 */
 (function() {
 	BCAPI.Config.TestServer = {
-		rootUrl: 'http://earth.bc.corp.adobe.com', // 'https://api-dc1.secure-earth.bc.corp.adobe.com',
-		siteId: 38604,	// bcapi-integration
-		username: 'bcadmin@bc1.local', //'bcapi-integration@mailinator.com',
-		password: 'password10', // '123456'
+		rootUrl: 'http://yoursite.com', // 'https://api-dc-id.worldsecuresystems.com',
+		siteId: 12345,	// bcapi-integration site id
+		username: 'email@you.com', //'your-email@yourdomain.com',
+		password: 'abcdefgh', // '123456'
 		accessToken: "",
 		genericToken: "",
 		tokenRequestTimeout: 60 * 1000

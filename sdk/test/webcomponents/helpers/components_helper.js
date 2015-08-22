f842347fe44446e845a857ff657e82f44764f612
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2015-08-22 18:33:39 +0300

    I added karma configuration for running unit tests.
    Initial configuration designed to run unit tests targetting newly added web components. Legacy unit tests are not run at this point.

---
+++ sdk/test/webcomponents/helpers/components_helper.js
@@ -0,0 +1,22 @@
+window.ComponentsHelper = (function() {
+    return {
+        /**
+         * This method provides a helper for executing test logic once a webcomponent is ready. It facilitates the 
+         * development of new unit tests which targets custom elements.
+         */
+        execWhenReady: function(compSelector, testLogic, done) {
+            var interval = setInterval(function() {
+                var comp = compSelector();
+
+                if (!comp._config) {
+                    return;
+                }
+
+                testLogic(comp);
+
+                clearInterval(interval);
+                done();
+            }, 100);
+        }
+    }
+})();

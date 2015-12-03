1d9e8fe14e5222b681e57e72746936fa021f6b04
Catalin Constantin Usurelu <catalin.usurelu5@gmail.com>
2015-12-03 15:32:29 +0000

    Removed monitor site and access token manual configuration

--- samples/bc-api-v3-monitor/_System/apps/bc-api-v3-monitor/assets/js/app.js
+++ samples/bc-api-v3-monitor/_System/apps/bc-api-v3-monitor/assets/js/app.js
@@ -4,10 +4,7 @@ window.MonitorApp = (function($) {
         init: function() {
             this._dataGrid = document.getElementById("gridMonitor");
 
-            BCAPI.Security.configure({
-                "siteUrl": "https://site4-2.localbc.worldsecuresystems.com",
-                "accessToken": "Bearer _bc_Q2xpZW50SWQ9cGFydG5lcjQtYWFhO1k2UGFQcTZlYnBnVlpkd0lYNlJ5UW42b2gwNlI5QjdLdTd4SUxQdEpzMXN1eFF4aVVYenB5N1JPYU56VFNzS3pMTW14MWtlVk05ZTFwSDRpRnZMSHRNVHR5VGF4SHdieFpBT1kybCtCOE5xQlpYbFJrTkljdnFQOHkybEJzUHZtSkR2bEpnc3VpaFFFeGhiV0pJMVV5NHRtUmlLa0xXY0ErVUo1VlREU0FpTXd4aS9TRlNSRkNuTS9wcmV2RUtEb2NrWDd4QytqNkJyYWlFRC9WSEM4bVFNYTd2cHhjcEJNSE43Q2xuZVQzZEQ1VEZremJTL1VRU1Y3ektuT1U2cUJFZHJGOG1yYUZPRVRYNW5zZ0VkYjloelNIWE1uLys0RndwNjIrVUZDUi93ZlhIMWRQQUJGMmxQTDRvVUdOeVF3WDQ3M3VSR1BnQjFaQ3lQdEswZ0tUelZtRWN3aitKQis3bUhLbmFxV3pvST0="
-            });
+            BCAPI.Security.configure(getBcConfig());
 
             this._dataGrid.configure();
         },
